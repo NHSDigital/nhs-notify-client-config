c97f05a5dff931f224376c345d41bf587e59389f
Airat Yusuff <airat.yusuff1@nhs.net>
2025-09-04 16:18:25 +0100

    update dataschema

--- packages/schemas/src/schemas/client-mutated-event.ts
+++ packages/schemas/src/schemas/client-mutated-event.ts
@@ -6,7 +6,7 @@ const $ClientMutatedEventData = z.union([$ClientWithAPIM, $ClientWithMESH]);
 
 const $ClientMutatedEventMetadata = $EventMetadata.extend({
   type: z.literal('uk.nhs.notify.config.ClientMutated.V1'),
-  dataschema: z.string().regex(/^https:\/\/notify\.nhs\.uk\/events\/schemas\/client-mutated-1.\d+\.\d+\.json$/),
+  dataschema: z.string().regex(/^https:\/\/notify\.nhs\.uk\/events\/schemas\/client-mutated\/1.\d+\.\d+\.json$/),
   dataschemaversion: z.string().regex(/^1\.\d+\.\d+$/), // Matches semantic versioning format with fixed major version
 });
 
