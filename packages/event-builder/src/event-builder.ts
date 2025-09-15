c8356fc0e3497960fe0519c4c9113384ce4a756f
Airat Yusuff <airat.yusuff1@nhs.net>
2025-09-15 16:09:05 +0100

    more updates with event renaming

--- packages/event-builder/src/event-builder.ts
+++ packages/event-builder/src/event-builder.ts
@@ -2,7 +2,7 @@ import { randomUUID } from "node:crypto";
 import {
   $ClientChangedEvent,
   ClientChangedEvent,
-} from "@nhsdigital/nhs-notify-config-schemas/src/schemas/client-mutated-event";
+} from "@nhsdigital/nhs-notify-config-schemas/src/schemas/client-changed-event";
 import { ClientInput } from "./input";
 import { eventSource } from "./config";
 
@@ -17,7 +17,7 @@ export const buildEvent = (input: ClientInput): ClientChangedEvent => {
     subject: input.clientId,
     type: "uk.nhs.notify.config.ClientChanged.v1",
     dataschema:
-      "https://notify.nhs.uk/events/schemas/client-mutated/1.0.0.json",
+      "https://notify.nhs.uk/events/schemas/client-changed/1.0.0.json",
     dataschemaversion: "1.0.0",
     data: {
       id: input.clientId,
--- packages/schemas/src/schemas/client-changed-event.ts
+++ packages/schemas/src/schemas/client-changed-event.ts
@@ -6,7 +6,7 @@ const $ClientChangedEventData = z.union([$ClientWithAPIM, $ClientWithMESH]);
 
 const $ClientChangedEventMetadata = $EventMetadata.extend({
   type: z.literal('uk.nhs.notify.config.ClientChanged.v1'),
-  dataschema: z.string().regex(/^https:\/\/notify\.nhs\.uk\/events\/schemas\/client-mutated\/1.\d+\.\d+\.json$/),
+  dataschema: z.string().regex(/^https:\/\/notify\.nhs\.uk\/events\/schemas\/client-changed\/1.\d+\.\d+\.json$/),
   dataschemaversion: z.string().regex(/^1\.\d+\.\d+$/), // Matches semantic versioning format with fixed major version
 });
 
