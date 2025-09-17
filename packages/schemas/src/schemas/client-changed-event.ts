109e20680885d55c646c8a1f231f09b9bf0e4ec0
Airat Yusuff <airat.yusuff1@nhs.net>
2025-09-17 11:01:04 +0100

    event type renaming: nhs.notify.config -> nhs.notify.client-config

--- packages/schemas/src/schemas/client-changed-event.ts
+++ packages/schemas/src/schemas/client-changed-event.ts
@@ -5,7 +5,7 @@ import { $ClientWithAPIM, $ClientWithMESH } from '../domain/client';
 const $ClientChangedEventData = z.union([$ClientWithAPIM, $ClientWithMESH]);
 
 const $ClientChangedEventMetadata = $EventMetadata.extend({
-  type: z.literal('uk.nhs.notify.config.ClientChanged.v1'),
+  type: z.literal('uk.nhs.notify.client-config.ClientChanged.v1'),
   dataschema: z.string().regex(/^https:\/\/notify\.nhs\.uk\/events\/schemas\/client-changed\/1.\d+\.\d+\.json$/),
   dataschemaversion: z.string().regex(/^1\.\d+\.\d+$/), // Matches semantic versioning format with fixed major version
 });
