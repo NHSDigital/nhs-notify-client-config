009e0b1c9174d3b133a700d7fe9f9c1f2b376257
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-09-17 10:43:31 +0100

    CCM-1190 fixing initial credential checks

--- infrastructure/terraform/components/ccapp/ssm_parameter_amplify_password.tf
+++ infrastructure/terraform/components/ccapp/ssm_parameter_amplify_password.tf
@@ -5,10 +5,10 @@ resource "aws_ssm_parameter" "amplify_password" {
   description = "The Basic Auth password used for the amplify app. This parameter is sourced from Github Environment variables"
 
   type  = "String"
-  value = var.AMPLIFY_BASIC_AUTH_SECRET != "unset" ? var.AMPLIFY_BASIC_AUTH_SECRET : random_password.password[0].result
+  value = var.AMPLIFY_BASIC_AUTH_SECRET != "unset" && length(var.AMPLIFY_BASIC_AUTH_SECRET) > 0 ? var.AMPLIFY_BASIC_AUTH_SECRET : random_password.amplify_password[0].result
 }
 
-resource "random_password" "password" {
+resource "random_password" "amplify_password" {
   count = var.enable_amplify_basic_auth && var.AMPLIFY_BASIC_AUTH_SECRET == "unset" ? 1 : 0
 
   length  = 16
--- infrastructure/terraform/components/ccapp/ssm_parameter_csrf_secret.tf
+++ infrastructure/terraform/components/ccapp/ssm_parameter_csrf_secret.tf
@@ -1,7 +1,14 @@
 resource "aws_ssm_parameter" "csrf_secret" {
   name        = "/${local.csi}/csrf_secret"
-  description = "cryptographic secret used for CSRF token generation"
+  description = "Cryptographic secret used for CSRF token generation"
 
   type  = "SecureString"
-  value = var.CSRF_SECRET
+  value = var.CSRF_SECRET != "unset" && length(var.CSRF_SECRET) > 0 ? var.CSRF_SECRET : random_password.csrf_secret[0].result
+}
+
+resource "random_password" "csrf_secret" {
+  count = var.enable_amplify_basic_auth && var.AMPLIFY_BASIC_AUTH_SECRET == "unset" ? 1 : 0
+
+  length  = 16
+  special = true
 }
