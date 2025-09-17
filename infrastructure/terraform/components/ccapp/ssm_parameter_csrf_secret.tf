277999f8c070e5a4398e90e65f54e3cbefd42a46
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-09-17 11:07:02 +0100

    CCM-1190 fixing initial credential checks

--- infrastructure/terraform/components/ccapp/ssm_parameter_csrf_secret.tf
+++ infrastructure/terraform/components/ccapp/ssm_parameter_csrf_secret.tf
@@ -3,11 +3,11 @@ resource "aws_ssm_parameter" "csrf_secret" {
   description = "Cryptographic secret used for CSRF token generation"
 
   type  = "SecureString"
-  value = var.CSRF_SECRET != "unset" && length(var.CSRF_SECRET) > 0 ? var.CSRF_SECRET : random_password.csrf_secret.result
+  value = var.CSRF_SECRET != "unset" || length(var.CSRF_SECRET) > 0 ? var.CSRF_SECRET : random_password.csrf_secret[0].result
 }
 
 resource "random_password" "csrf_secret" {
-  count = var.enable_amplify_basic_auth && var.AMPLIFY_BASIC_AUTH_SECRET == "unset" ? 1 : 0
+  count = var.enable_amplify_basic_auth && (var.CSRF_SECRET == "unset" || length(var.CSRF_SECRET) == 0) ? 1 : 0
 
   length  = 16
   special = true
