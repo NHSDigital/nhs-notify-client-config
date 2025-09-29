5598edbabd71bec264f2177b13ea7cf94e935051
Mike Houston <michael.houston4@nhs.net>
2025-09-29 10:44:35 +0100

    CCM-12273: update git ignore and build scripts

--- packages/schemas/src/cli/generate-json.ts
+++ packages/schemas/src/cli/generate-json.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 import * as fs from "node:fs";
-import { $ClientBase } from "src/domain/client";
-import { $CampaignBase } from "src/domain/campaign";
-import { $Campaign, $Client } from "src/domain";
+import { $ClientBase } from "../domain/client";
+import { $CampaignBase } from "../domain/campaign";
+import { $Campaign, $Client } from "../domain";
 import {
   $ClientSubscription,
   $ClientSubscriptionBase,
-} from "src/domain/client-subscription";
+} from "../domain/client-subscription";
 
 for (const [key, schema] of Object.entries({
   client: $Client,
@@ -21,6 +21,7 @@ for (const [key, schema] of Object.entries({
     target: "openapi-3.0",
     reused: "ref",
   });
+  fs.mkdirSync("client-config/domain", { recursive: true });
   const file = `client-config/domain/${key}.json`;
   fs.writeFileSync(file, JSON.stringify(json, null, 2));
   console.info(`Wrote JSON schema for ${key} to ${file}`);
