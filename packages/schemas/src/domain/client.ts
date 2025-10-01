fb15dd7627763b0e41f326e84ffe0a41be11eaba
Mike Houston <michael.houston4@nhs.net>
2025-10-01 14:26:17 +0100

    CCM-12273: Add description for possible RFR codes

--- packages/schemas/src/domain/client.ts
+++ packages/schemas/src/domain/client.ts
@@ -1,11 +1,11 @@
-import { z } from "zod";
-import { $MeshMailbox } from "./mesh-mailbox";
-import { $ApimApplication } from "./apim-application";
-import { $SuppressionFilter } from "./suppression-filter";
-import { $ClientQuota } from "./client-quota";
-import { $Environment, ConfigBase } from "./common";
-import { $FeatureFlag } from "./feature-flag";
-import { idRef } from "../helpers/id-ref";
+import {z} from "zod";
+import {$MeshMailbox} from "./mesh-mailbox";
+import {$ApimApplication} from "./apim-application";
+import {$SuppressionFilter} from "./suppression-filter";
+import {$ClientQuota} from "./client-quota";
+import {$Environment, ConfigBase} from "./common";
+import {$FeatureFlag} from "./feature-flag";
+import {idRef} from "../helpers/id-ref";
 import rfrCoding from "./rfr-coding";
 
 export const $ClientBase = ConfigBase("Client")
@@ -26,7 +26,22 @@ export const $Client = $ClientBase
     apimApplication: $ApimApplication.optional(),
 
     featureFlags: z.array(idRef($FeatureFlag)).optional(),
-    rfrCodes: z.array(z.enum(Object.keys(rfrCoding))).optional(),
+    rfrCodes: z.array(z.enum(Object.keys(rfrCoding))).optional().meta({
+      title: "Reason for Removal Codes",
+      description:
+        `When sending a communication to a recipient, the reason for removal codes (RFR codes) indicate
+why a recipient has been removed from their GP's registered patient list.
+See https://data.developer.nhs.uk/dms/mim/6.3.01/Vocabulary/NHAISRemovalReasonCode.htm for information on possible codes.
+
+PDS currently only provides a subset of 'Exit' codes, including those for Deceased patients.
+* DEA - Death
+* EMB - Embarkation
+* SCT - Transferred to Scotland
+* NIT - Transferred to Northern Ireland
+* TRA - Temporary resident not returned
+* ORR - Other reason
+`,
+    }),
     suppressionFilters: z.array($SuppressionFilter).optional(),
   })
   .meta({
